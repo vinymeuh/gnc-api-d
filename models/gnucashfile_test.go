90ab8f554e627237b0b0a7aa242ac5e7c2a3d05f
vinymeuh <vinymeuh@gmail.com>
2019-06-13 23:14:51 +0200

    add tests for transactions

--- models/gnucashfile_test.go
+++ models/gnucashfile_test.go
@@ -18,5 +18,16 @@ func TestLoadGnuCashFile(t *testing.T) {
 
 		alls := root[0].All()
 		assert.Equal(t, len(index), len(alls), "accounts index is incoherent")
+
+		books := data.FindByName("Books")
+		assert.Equal(t, 1, len(books), "Problem while retrieve the account 'Books'")
+
+		actBooks := books[0]
+		assert.Equal(t, "97c2d5b268164b479944e221ae0267f1", actBooks.ID, "Problem with 'Books' account ID")
+		assert.Equal(t, 1, len(actBooks.Transactions), "Problem with 'Books' account transactions")
+
+		trnBooks := actBooks.Transactions[0]
+		assert.Equal(t, "2019-06-10", trnBooks.Date, "Problem with 'Books' transaction date")
+		assert.Equal(t, 30.05, trnBooks.Value, "Problem with 'Books' transaction value")
 	}
 }
