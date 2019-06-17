9993542f989488157ef6d611042cdc171c87d219
vinymeuh <vinymeuh@gmail.com>
2019-06-17 21:52:08 +0200

    code refactoring (log)

---
+++ log.go
@@ -0,0 +1,32 @@
+// Copyright 2019 VinyMeuh. All rights reserved.
+// Use of the source code is governed by a MIT-style license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+	"time"
+)
+
+type writer struct {
+	io.Writer
+}
+
+func (w writer) Write(b []byte) (n int, err error) {
+	return w.Writer.Write(append([]byte(time.Now().Format("2006-01-02T15:04:05-07:00 ")), b...))
+}
+
+func setupLogFile() {
+	log.SetFlags(log.Lshortfile)
+	log.SetOutput(writer{os.Stdout})
+	if logFilePath := getLogFilePath(); logFilePath != "" {
+		f, err := os.OpenFile(logFilePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		log.SetOutput(writer{f})
+	}
+}
--- main.go
+++ main.go
@@ -4,11 +4,9 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/vinymeuh/gnc-api-d/api"
 	"github.com/vinymeuh/gnc-api-d/models"
@@ -30,26 +28,8 @@ func getGnuCashFile() string {
 	return os.Getenv("GNUCASH_FILE_PATH")
 }
 
-type writer struct {
-	io.Writer
-}
-
-func (w writer) Write(b []byte) (n int, err error) {
-	return w.Writer.Write(append([]byte(time.Now().Format("2006-01-02T15:04:05-07:00 ")), b...))
-}
-
 func main() {
-	// setup logging
-	log.SetFlags(log.Lshortfile)
-	log.SetOutput(writer{os.Stdout})
-	if logFilePath := getLogFilePath(); logFilePath != "" {
-		f, err := os.OpenFile(logFilePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer f.Close()
-		log.SetOutput(writer{f})
-	}
+	setupLogFile()
 
 	// load Gnucash data
 	gncfile := getGnuCashFile()
