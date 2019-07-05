140b7c7e73d1099f57f095c49b29e57d12bbc140
vinymeuh <vinymeuh@gmail.com>
2019-07-05 20:04:38 +0200

    enhance tests for router

--- api/router_test.go
+++ api/router_test.go
@@ -9,37 +9,47 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/vinymeuh/gnc-api-d/models"
 )
 
-var validRoutes = []string{
-	"/",
-	//"/accounts",
-	//"/accounttypes",
-	"/balance",
+var routesToTest = []struct {
+	method string
+	path   string
+	status int
+}{
+	{"GET", "/", http.StatusOK},
+	{"GET", "/accounts", http.StatusOK},
+	{"GET", "/accounttypes", http.StatusOK},
+	{"GET", "/balance", http.StatusOK},
+	{"GET", "/not-exists", http.StatusNotFound},
+	{"POST", "/", http.StatusMethodNotAllowed},
 }
 
-func TestValidRoutes(t *testing.T) {
-	r := NewRouter(nil)
+func TestRoutes(t *testing.T) {
+
+	var root = models.Account{
+		ID:   "0",
+		Name: "Dummy",
+		Type: "ROOT",
+	}
+	r := NewRouter(&root)
 	ts := httptest.NewServer(r)
 	defer ts.Close()
 
-	for _, tt := range validRoutes {
-		res, err := http.Get(ts.URL + tt)
+	var res *http.Response
+	var err error
+	for _, tt := range routesToTest {
+		switch tt.method {
+		case "GET":
+			res, err = http.Get(ts.URL + tt.path)
+		case "POST":
+			res, err = http.Post(ts.URL+tt.path, "text/plain", nil)
+		default:
+			t.Fatalf("Testing HTTP method %s is not supported", tt.method)
+		}
 		if err != nil {
 			t.Fatal(err)
 		}
-		assert.Equal(t, http.StatusOK, res.StatusCode, "Status code for %s is wrong.", tt)
+		assert.Equal(t, tt.status, res.StatusCode, "Status code for %s is wrong.", tt)
 	}
-}
-
-func TestInvalidRoutes(t *testing.T) {
-	r := NewRouter(nil)
-	ts := httptest.NewServer(r)
-	defer ts.Close()
-
-	res, err := http.Get(ts.URL + "/not-exists")
-	if err != nil {
-		t.Fatal(err)
-	}
-	assert.Equal(t, http.StatusNotFound, res.StatusCode, "Status code for /not-exists is wrong.")
 }
